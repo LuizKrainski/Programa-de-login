0aead68f4d7fc9ffd71e26028086cde2c40579fe
Luiz <luiz.krainski@fkn.com.br>
2022-11-24 11:53:26 -0300

    Stage com terminal feito

--- Stage.py
+++ Stage.py
@@ -1,47 +1,47 @@
-def log_in():
-    username = input("Por favor insira seu nome de usuário : ")
-    password = input("Por favor insira sua senha : ")
+def login():
+    usuario = input("Por favor insira seu nome de usuário : ")
+    senha = input("Por favor insira sua senha : ")
 
     with open('test_file.txt', 'r') as file:
         for line in file:
-            if line == 'Usuario:{0}, Senha:{1}'.format(username, password):
-                print ("Saudações," , username, "Agora você está logado")
-                return True, username, password
+            if line == 'Usuario:{0}, Senha:{1}'.format(usuario, senha):
+                print ("Saudações," , usuario, "Agora você está logado")
+                return True, usuario, senha
     print (" Desculpe, nome de usuário e senha incorretos, digite novamente para validação ")
     return False, '', ''
 
-def new_user():
+def novo_usuario():
     succes = False
     while not succes:
-        new_user = input("Por favor, digite seu novo nome de usuário : ")
-        new_pass = input("Por favor, digite sua nova senha : ")
+        novo_usuario = input("Por favor, digite seu novo nome de usuário : ")
+        nova_senha = input("Por favor, digite sua nova senha : ")
 
         exists = False
         with open("test_file.txt","r") as file:
             for line in file:
-                if line.split(',')[0] == 'Usuario:'+new_user:
-                    print ('Nome de usuário Inválido: {0} já existe'.format(new_user))
+                if line.split(',')[0] == 'Usuario:'+novo_usuario:
+                    print ('Nome de usuário Inválido: {0} já existe'.format(novo_usuario))
                     exists = True
 
         if not exists:
             with open("test_file.txt","a") as file:
-                file.write('Usuario:{0}, Senha:{1}'.format(new_user, new_pass))
+                file.write('Usuario:{0}, Senha:{1}'.format(novo_usuario, nova_senha))
             succes = True
-    print ('Você criou um novo usuário com nome de usuário:{0} e senha:{1}'.format(new_user, new_pass))
+    print ('Você criou um novo usuário com nome de usuário:{0} e senha:{1}'.format(novo_usuario, nova_senha))
 
 def main():
-    command = username = password = ''
+    command = usuario = senha = ''
     logged_in = False
     while command != 'Fechar':
         command = input('Escreva o comando: ')
         if command == 'Conecte-se':
-            logged_in, username, passowrd = log_in()
+            logged_in, usuario, senha = login()
         if command == 'Sair':
             logged_in = False
-            username = passowrd = ''
+            usuario = senha = ''
         if command == 'Novo usuario':
             if not logged_in:
-                new_user()
+                novo_usuario()
             else:
                 print ('Primeiro logout para criar um novo usuário')
 
